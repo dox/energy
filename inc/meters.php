2f86e18175457d34c337df12837aa97caddf9130
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-01-27 08:41:05 +0000

    Design changes to meter card
    Design changes to meter card

--- inc/meters.php
+++ inc/meters.php
@@ -100,5 +100,40 @@ public function allByLocation($locationUID = null) {
 	return $meters;
 }
 
+public function displayMeterCard($uid = null) {
+	$this->meterUID = $uid;
+	$meter = $this->getOne();
+	
+	$output  = "<div class=\"col-md-4\">";
+		$output .= "<div class=\"card mb-4 shadow-sm\" >";
+			if (isset($meter['photograph'])) {
+				$img = "uploads/" . $meter['photograph'];
+				$output .= "<img class=\"bd-placeholder-img card-img-top\" src=\"" . $img . "\" width=\"100%\" height=\"225\">";
+			} else {
+				$output .= "<svg class=\"bd-placeholder-img card-img-top\" width=\"100%\" height=\"225\" xmlns=\"http://www.w3.org/2000/svg\" preserveAspectRatio=\"xMidYMid slice\" focusable=\"false\" role=\"img\" \"><rect width=\"100%\" height=\"100%\" fill=\"#55595c\"/></svg>";
+			}
+			$output .= "<div class=\"card-img-overlay\"><h5 class=\"card-title\">" . $meter['name'] . "</h5><p class=\"card-text\"><samp>" . $meter['serial'] . "</samp></p></div>";
+			$output .= "<div class=\"card-body\">";
+				$output .= "<div class=\"d-flex justify-content-between align-items-center\">";
+					$output .= "<div class=\"btn-group\">";
+						$output .= "<a href=\"index.php?n=meter&meterUID=" . $meter['uid'] . "\" class=\"btn btn-sm btn-outline-secondary\">View</a>";
+						$output .= "<a href=\"index.php?n=meter_edit&meterUID=" . $meter['uid'] . "\" class=\"btn btn-sm btn-outline-secondary\">Edit</a>";
+					$output .= "</div>";
+					$output .= "<small class=\"text-muted\">" . $meter['type'] . "</small>";
+				$output .= "</div>";
+				
+				if ($this->daysSinceLastUpdate($meter['uid']) > 80) {
+					$output .= "<span class=\"badge badge-danger\">Updated " . $this->daysSinceLastUpdate($meter['uid']) . " days ago</span>";
+				} else if ($this->daysSinceLastUpdate($meter['uid']) > 40) {
+					$output .= "<span class=\"badge badge-warning\">Updated " . $this->daysSinceLastUpdate($meter['uid']) . " days ago</span>";
+				} else if ($this->daysSinceLastUpdate($meter['uid']) < 30) {
+					$output .= "<span class=\"badge badge-primary\">Updated " . $this->daysSinceLastUpdate($meter['uid']) . " days ago</span>";
+				}
+			$output .= "</div>";	
+		$output .= "</div>";
+	$output .= "</div>";
+	
+	return $output;
+}
 } //end CLASS
 ?>
