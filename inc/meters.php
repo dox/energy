71be335691adce807d9a0f4e8baf1bcc307b5b7f
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-06-01 12:26:15 +0100

    Catching no last update

--- inc/meters.php
+++ inc/meters.php
@@ -5,18 +5,18 @@ public $meterUID;
 
 public function thisMeterUnits($type = null) {
 	global $db;
-	
-	
-	
+
+
+
 	if ($type == null) {
 		$meter = $db->where("uid", $this->meterUID);
 		$meter = $db->getOne("meters");
-		
+
 		$meterType = $meter['type'];
 	} else {
 		$meterType = $type;
 	}
-	
+
 	if ($meterType == "Electric") {
 		$unit = "kWh";
 	} elseif ($meterType == "Gas") {
@@ -31,35 +31,35 @@ public function thisMeterUnits($type = null) {
 
 public function daysSinceLastUpdate($uid = null) {
 	global $db;
-	
+
 	$meter = $db->where("uid", $uid);
 	$meter = $db->getOne("meters");
-	
+
 	$lastReading = $db->where("meter", $meter['uid']);
 	$lastReading = $db->orderBy("date", "DESC");
 	$lastReading = $db->getOne("readings");
-	
+
 	if (isset($lastReading)) {
 		$dt1 = date('Y-m-d');
 		$dt2 = date('Y-m-d', strtotime($lastReading['date']));
-		
+
 		$y1 = substr($dt1,0,4);
 		$m1 = substr($dt1,5,2);
 		$d1 = substr($dt1,8,2);
 		$h1 = substr($dt1,11,2);
 		$i1 = substr($dt1,14,2);
-		$s1 = substr($dt1,17,2);    
-		
+		$s1 = substr($dt1,17,2);
+
 		$y2 = substr($dt2,0,4);
 		$m2 = substr($dt2,5,2);
 		$d2 = substr($dt2,8,2);
 		$h2 = substr($dt2,11,2);
 		$i2 = substr($dt2,14,2);
-		$s2 = substr($dt2,17,2);    
-		
+		$s2 = substr($dt2,17,2);
+
 		$r1=date('U',mktime($h1,$i1,$s1,$m1,$d1,$y1));
 		$r2=date('U',mktime($h2,$i2,$s2,$m2,$d2,$y2));
-		
+
 		$differenceInSeconds = ($r1 - $r2);
 		$differenceInMins = ($differenceInSeconds)/60;
 		$differenceInHours = ($differenceInMins)/60;
@@ -67,51 +67,53 @@ public function daysSinceLastUpdate($uid = null) {
 	} else {
 		$differenceInDays = 0;
 	}
-	
-	
+
+
 	return round($differenceInDays);
 }
 
 public function getOne() {
 	global $db;
-	
+
 	$meter = $db->where("uid", $this->meterUID);
 	$meter = $db->getOne("meters");
-	
+
 	return $meter;
 }
 
 public function all() {
 	global $db;
-	
+
 	$meters = $db->orderBy('name', "DESC");
 	$meters = $db->get("meters");
-	
+
 	return $meters;
 }
 
 public function allByLocation($locationUID = null) {
 	global $db;
-	
+
 	$meters = $db->where("location", $locationUID);
 	$meters = $db->orderBy('name', "DESC");
 	$meters = $db->get("meters");
-	
+
 	return $meters;
 }
 
 public function displayMeterCard($uid = null) {
 	$this->meterUID = $uid;
 	$meter = $this->getOne();
-	
+
 	if ($this->daysSinceLastUpdate($meter['uid']) > 80) {
 		$lastUpdated = "<span class=\"badge badge-danger\">Updated " . $this->daysSinceLastUpdate($meter['uid']) . " days ago</span>";
 	} else if ($this->daysSinceLastUpdate($meter['uid']) > 40) {
 		$lastUpdated = "<span class=\"badge badge-warning\">Updated " . $this->daysSinceLastUpdate($meter['uid']) . " days ago</span>";
 	} else if ($this->daysSinceLastUpdate($meter['uid']) < 30) {
 		$lastUpdated = "<span class=\"badge badge-primary\">Updated " . $this->daysSinceLastUpdate($meter['uid']) . " days ago</span>";
+	} else {
+		$lastUpdated = "<span class=\"badge badge-primary\">Updated " . $this->daysSinceLastUpdate($meter['uid']) . " days ago</span>";
 	}
-				
+
 	$output  = "<div class=\"col-md-4\">";
 		$output .= "<div class=\"card mb-4 shadow-sm\" >";
 			if (isset($meter['photograph'])) {
@@ -130,12 +132,12 @@ public function displayMeterCard($uid = null) {
 					$output .= "</div>";
 					$output .= "<small class=\"text-muted\">" . $meter['type'] . " / <samp>" . $meter['serial'] . "</samp></small>";
 				$output .= "</div>";
-				
-				
-			$output .= "</div>";	
+
+
+			$output .= "</div>";
 		$output .= "</div>";
 	$output .= "</div>";
-	
+
 	return $output;
 }
 } //end CLASS
