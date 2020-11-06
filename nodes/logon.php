f56b1d706bf15017c2a121b794b41da6dd22e0b6
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-11-06 16:54:42 +0000

    Adding reset link to logon

--- nodes/logon.php
+++ nodes/logon.php
@@ -1,23 +1,24 @@
 <div class="container">
   <main class="form-signin">
     <form method="post" id="loginSubmit" action="index.php">
-    <!--<img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">-->
-    <h1 class="h3 mb-3 font-weight-normal">Please Log In</h1>
-    <label for="inputUsername" class="visually-hidden">Username</label>
-    <input type="text" id="inputUsername" name="inputUsername" class="form-control" placeholder="Username" required autofocus>
-    <label for="inputPassword" class="visually-hidden">Password</label>
-    <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password" required>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">Forgot your password?</p>
-  </form>
-</main>
+      <div class="mb-4 text-center">
+        <svg width="72" height="54" viewBox="0 0 16 16" class="bi bi-lightning" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09zM4.157 8.5H7a.5.5 0 0 1 .478.647L6.11 13.59l5.732-6.09H9a.5.5 0 0 1-.478-.647L9.89 2.41 4.157 8.5z"/>
+        </svg>
+
+        <h1 class="h3 mb-3 font-weight-normal">Please Log In</h1>
+      </div>
+      <label for="inputUsername" class="visually-hidden">Username</label>
+      <input type="text" id="inputUsername" name="inputUsername" class="form-control" placeholder="Username" required autofocus>
+      <label for="inputPassword" class="visually-hidden">Password</label>
+      <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password" required>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted text-center"><a href="<?php echo reset_url; ?>">Forgot your password?</a></p>
+    </form>
+  </main>
 <div>
 
 <style>
-
-
-
-
 .form-signin {
   width: 100%;
   max-width: 330px;
