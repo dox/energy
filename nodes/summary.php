ac1eca5393df6044a776469a574e8229f3b2f73a
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-11-25 09:31:28 +0000

    Years now auto-roll

--- nodes/summary.php
+++ nodes/summary.php
@@ -8,300 +8,169 @@ $locations = $locations->all();
 ?>
 
 <div class="container">
-<h1>Electricity</h1>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col" style="width: 400px;"></th>
-      <th scope="col">2015</th>
-      <th scope="col">2016</th>
-      <th scope="col">2017</th>
-      <th scope="col">2018</th>
-      <th scope="col">2019</th>
-      <th scope="col">2020</th>
-    </tr>
-</thead>
-	<tbody>
-		<?php
-		foreach ($locations AS $location) {
-			$metersClass = new meters;
-			$meters = $metersClass->allByLocationAndType($location['uid'], 'Electric');
+  <h1>Electricity</h1>
 
-			$meterMAXArray2020 = null;
-			$meterMAXArray2019 = null;
-			$meterMAXArray2018 = null;
-			$meterMAXArray2017 = null;
-			$meterMAXArray2016 = null;
-			$meterMAXArray2015 = null;
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col" style="width: 400px;"></th>
+        <?php
+        $i = 5;
 
-			foreach ($meters AS $meter) {
-				$meterMAX2020 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2020' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2019 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2019' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2018 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2018' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2017 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2017' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2016 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2016' ORDER BY reading1 DESC")->fetchAll();
-        $meterMAX2015 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2015' ORDER BY reading1 DESC")->fetchAll();
-        $meterMAX2014 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2014' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAXArray2020[] = $meterMAX2020[0]['reading1'];
-				$meterMAXArray2019[] = $meterMAX2019[0]['reading1'];
-				$meterMAXArray2018[] = $meterMAX2018[0]['reading1'];
-				$meterMAXArray2017[] = $meterMAX2017[0]['reading1'];
-				$meterMAXArray2016[] = $meterMAX2016[0]['reading1'];
-        $meterMAXArray2015[] = $meterMAX2015[0]['reading1'];
-        $meterMAXArray2014[] = $meterMAX2014[0]['reading1'];
-			}
+        do {
+          $year = date('Y', strtotime($i . " year ago"));
 
-			if (isset($meterMAXArray2020)) {
-				$arraySum2020 = array_sum($meterMAXArray2020);
-			} else {
-				$arraySum2020 = 0;
-			}
-			if (isset($meterMAXArray2019)) {
-				$arraySum2019 = array_sum($meterMAXArray2019);
-			} else {
-				$arraySum2019 = 0;
-			}
-			if (isset($meterMAXArray2018)) {
-				$arraySum2018 = array_sum($meterMAXArray2018);
-			} else {
-				$arraySum2018 = 0;
-			}
-			if (isset($meterMAXArray2017)) {
-				$arraySum2017 = array_sum($meterMAXArray2017);
-			} else {
-				$arraySum2017 = 0;
-			}
-			if (isset($meterMAXArray2016)) {
-				$arraySum2016 = array_sum($meterMAXArray2016);
-			} else {
-				$arraySum2016 = 0;
-			}
-			if (isset($meterMAXArray2015)) {
-				$arraySum2015 = array_sum($meterMAXArray2015);
-			} else {
-				$arraySum2015 = 0;
-			}
+          echo "<th scope=\"col\">" . $year . "</th>";
+          $i--;
+        } while ($i >= 0);
 
-      //calculate difference/consumption
-      $arraySum2020 = $arraySum2020 - $arraySum2019;
-      $arraySum2019 = $arraySum2019 - $arraySum2018;
-      $arraySum2018 = $arraySum2018 - $arraySum2017;
-      $arraySum2017 = $arraySum2017 - $arraySum2016;
-      $arraySum2016 = $arraySum2016 - $arraySum2015;
-      $arraySum2015 = $arraySum2015 - $arraySum2014;
+        ?>
+      </tr>
+      <?php
+      foreach ($locations AS $location) {
+        $metersClass = new meters;
+  			$meters = $metersClass->allByLocationAndType($location['uid'], 'Electric');
 
-			$output  = "<tr>";
-			$output .= "<th scope=\"row\">" . $location['name'] . "</th>";
-      $output .= "<td>" . number_format($arraySum2015) . "</td>";
-			$output .= "<td>" . number_format($arraySum2016) . "</td>";
-			$output .= "<td>" . number_format($arraySum2017) . "</td>";
-			$output .= "<td>" . number_format($arraySum2018) . "</td>";
-			$output .= "<td>" . number_format($arraySum2019) . "</td>";
-      $output .= "<td>" . number_format($arraySum2020) . "</td>";
-			$output .= "</tr>";
+        echo "<tr>";
 
-			echo $output;
-		}
-		?>
-  </tbody>
-</table>
+        echo "<th scope=\"row\">" . $location['name'] . "</th>";
+
+        $i = 5;
+        do {
+          $previousYear = date('Y', strtotime($i + 1 . " years ago"));
+          $year = date('Y', strtotime($i . " year ago"));
+
+          $meterMAXArrayPrevious = array();
+          $meterMAXArray = array();
+          foreach ($meters AS $meter) {
+            //echo "SELECT MAX(reading1) AS reading1 from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '" . $previousYear . "'";
+            $meterMAXPrevious = $db->query("SELECT MAX(reading1) AS reading1 from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '" . $previousYear . "'")->fetchAll();
+            $meterMAX = $db->query("SELECT MAX(reading1) AS reading1 from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '" . $year . "'")->fetchAll();
+            $meterMAXArrayPrevious[] = $meterMAXPrevious[0]['reading1'];
+            $meterMAXArray[] = $meterMAX[0]['reading1'];
+          }
+
+          $arraySum = array_sum($meterMAXArray) - array_sum($meterMAXArrayPrevious);
+
+          echo "<td>" . number_format($arraySum) . "</td>";
+          $i--;
+        } while ($i >= 0);
+
+  			echo "</tr>";
+      }
+      ?>
+    </thead>
+  </table>
 </div>
 
 <div class="container">
-<h1>Gas</h1>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col" style="width: 400px;"></th>
-      <th scope="col">2015</th>
-      <th scope="col">2016</th>
-      <th scope="col">2017</th>
-      <th scope="col">2018</th>
-      <th scope="col">2019</th>
-      <th scope="col">2020</th>
-    </tr>
-</thead>
-	<tbody>
-		<?php
-		foreach ($locations AS $location) {
-			$metersClass = new meters;
-			$meters = $metersClass->allByLocationAndType($location['uid'], 'Gas');
+  <h1>Gas</h1>
 
-			$meterMAXArray2020 = null;
-			$meterMAXArray2019 = null;
-			$meterMAXArray2018 = null;
-			$meterMAXArray2017 = null;
-			$meterMAXArray2016 = null;
-			$meterMAXArray2015 = null;
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col" style="width: 400px;"></th>
+        <?php
+        $i = 5;
 
-			foreach ($meters AS $meter) {
-        $meterMAX2020 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2020' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2019 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2019' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2018 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2018' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2017 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2017' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2016 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2016' ORDER BY reading1 DESC")->fetchAll();
-        $meterMAX2015 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2015' ORDER BY reading1 DESC")->fetchAll();
-        $meterMAX2014 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2014' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAXArray2020[] = $meterMAX2020[0]['reading1'];
-				$meterMAXArray2019[] = $meterMAX2019[0]['reading1'];
-				$meterMAXArray2018[] = $meterMAX2018[0]['reading1'];
-				$meterMAXArray2017[] = $meterMAX2017[0]['reading1'];
-				$meterMAXArray2016[] = $meterMAX2016[0]['reading1'];
-        $meterMAXArray2015[] = $meterMAX2015[0]['reading1'];
-        $meterMAXArray2014[] = $meterMAX2014[0]['reading1'];
-			}
+        do {
+          $year = date('Y', strtotime($i . " year ago"));
 
-			if (isset($meterMAXArray2020)) {
-				$arraySum2020 = array_sum($meterMAXArray2020);
-			} else {
-				$arraySum2020 = 0;
-			}
-			if (isset($meterMAXArray2019)) {
-				$arraySum2019 = array_sum($meterMAXArray2019);
-			} else {
-				$arraySum2019 = 0;
-			}
-			if (isset($meterMAXArray2018)) {
-				$arraySum2018 = array_sum($meterMAXArray2018);
-			} else {
-				$arraySum2018 = 0;
-			}
-			if (isset($meterMAXArray2017)) {
-				$arraySum2017 = array_sum($meterMAXArray2017);
-			} else {
-				$arraySum2017 = 0;
-			}
-			if (isset($meterMAXArray2016)) {
-				$arraySum2016 = array_sum($meterMAXArray2016);
-			} else {
-				$arraySum2016 = 0;
-			}
-			if (isset($meterMAXArray2015)) {
-				$arraySum2015 = array_sum($meterMAXArray2015);
-			} else {
-				$arraySum2015 = 0;
-			}
+          echo "<th scope=\"col\">" . $year . "</th>";
+          $i--;
+        } while ($i >= 0);
 
-      //calculate difference/consumption
-      $arraySum2020 = $arraySum2020 - $arraySum2019;
-      $arraySum2019 = $arraySum2019 - $arraySum2018;
-      $arraySum2018 = $arraySum2018 - $arraySum2017;
-      $arraySum2017 = $arraySum2017 - $arraySum2016;
-      $arraySum2016 = $arraySum2016 - $arraySum2015;
-      $arraySum2015 = $arraySum2015 - $arraySum2014;
+        ?>
+      </tr>
+      <?php
+      foreach ($locations AS $location) {
+        $metersClass = new meters;
+        $meters = $metersClass->allByLocationAndType($location['uid'], 'Gas');
 
-			$output  = "<tr>";
-			$output .= "<th scope=\"row\">" . $location['name'] . "</th>";
-      $output .= "<td>" . number_format($arraySum2015) . "</td>";
-			$output .= "<td>" . number_format($arraySum2016) . "</td>";
-			$output .= "<td>" . number_format($arraySum2017) . "</td>";
-			$output .= "<td>" . number_format($arraySum2018) . "</td>";
-			$output .= "<td>" . number_format($arraySum2019) . "</td>";
-      $output .= "<td>" . number_format($arraySum2020) . "</td>";
-			$output .= "</tr>";
+        echo "<tr>";
 
-			echo $output;
-		}
-		?>
-  </tbody>
-</table>
+        echo "<th scope=\"row\">" . $location['name'] . "</th>";
+
+        $i = 5;
+        do {
+          $previousYear = date('Y', strtotime($i + 1 . " years ago"));
+          $year = date('Y', strtotime($i . " year ago"));
+
+          $meterMAXArrayPrevious = array();
+          $meterMAXArray = array();
+          foreach ($meters AS $meter) {
+            //echo "SELECT MAX(reading1) AS reading1 from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '" . $previousYear . "'";
+            $meterMAXPrevious = $db->query("SELECT MAX(reading1) AS reading1 from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '" . $previousYear . "'")->fetchAll();
+            $meterMAX = $db->query("SELECT MAX(reading1) AS reading1 from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '" . $year . "'")->fetchAll();
+            $meterMAXArrayPrevious[] = $meterMAXPrevious[0]['reading1'];
+            $meterMAXArray[] = $meterMAX[0]['reading1'];
+          }
+
+          $arraySum = array_sum($meterMAXArray) - array_sum($meterMAXArrayPrevious);
+
+          echo "<td>" . number_format($arraySum) . "</td>";
+          $i--;
+        } while ($i >= 0);
+
+        echo "</tr>";
+      }
+      ?>
+    </thead>
+  </table>
 </div>
 
 <div class="container">
-<h1>Water</h1>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col" style="width: 400px;"></th>
-      <th scope="col">2015</th>
-      <th scope="col">2016</th>
-      <th scope="col">2017</th>
-      <th scope="col">2018</th>
-      <th scope="col">2019</th>
-      <th scope="col">2020</th>
-    </tr>
-</thead>
-	<tbody>
-		<?php
-		foreach ($locations AS $location) {
-			$metersClass = new meters;
-			$meters = $metersClass->allByLocationAndType($location['uid'], 'Water');
+  <h1>Water</h1>
 
-			$meterMAXArray2020 = null;
-			$meterMAXArray2019 = null;
-			$meterMAXArray2018 = null;
-			$meterMAXArray2017 = null;
-			$meterMAXArray2016 = null;
-			$meterMAXArray2015 = null;
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col" style="width: 400px;"></th>
+        <?php
+        $i = 5;
 
-			foreach ($meters AS $meter) {
-        $meterMAX2020 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2020' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2019 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2019' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2018 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2018' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2017 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2017' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAX2016 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2016' ORDER BY reading1 DESC")->fetchAll();
-        $meterMAX2015 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2015' ORDER BY reading1 DESC")->fetchAll();
-        $meterMAX2014 = $db->query("SELECT * from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '2014' ORDER BY reading1 DESC")->fetchAll();
-				$meterMAXArray2020[] = $meterMAX2020[0]['reading1'];
-				$meterMAXArray2019[] = $meterMAX2019[0]['reading1'];
-				$meterMAXArray2018[] = $meterMAX2018[0]['reading1'];
-				$meterMAXArray2017[] = $meterMAX2017[0]['reading1'];
-				$meterMAXArray2016[] = $meterMAX2016[0]['reading1'];
-				$meterMAXArray2014[] = $meterMAX2015[0]['reading1'];
-			}
+        do {
+          $year = date('Y', strtotime($i . " year ago"));
 
-			if (isset($meterMAXArray2020)) {
-				$arraySum2020 = array_sum($meterMAXArray2020);
-			} else {
-				$arraySum2020 = 0;
-			}
-			if (isset($meterMAXArray2019)) {
-				$arraySum2019 = array_sum($meterMAXArray2019);
-			} else {
-				$arraySum2019 = 0;
-			}
-			if (isset($meterMAXArray2018)) {
-				$arraySum2018 = array_sum($meterMAXArray2018);
-			} else {
-				$arraySum2018 = 0;
-			}
-			if (isset($meterMAXArray2017)) {
-				$arraySum2017 = array_sum($meterMAXArray2017);
-			} else {
-				$arraySum2017 = 0;
-			}
-			if (isset($meterMAXArray2016)) {
-				$arraySum2016 = array_sum($meterMAXArray2016);
-			} else {
-				$arraySum2016 = 0;
-			}
-			if (isset($meterMAXArray2015)) {
-				$arraySum2015 = array_sum($meterMAXArray2015);
-			} else {
-				$arraySum2015 = 0;
-			}
+          echo "<th scope=\"col\">" . $year . "</th>";
+          $i--;
+        } while ($i >= 0);
 
-      //calculate difference/consumption
-      $arraySum2020 = $arraySum2020 - $arraySum2019;
-      $arraySum2019 = $arraySum2019 - $arraySum2018;
-      $arraySum2018 = $arraySum2018 - $arraySum2017;
-      $arraySum2017 = $arraySum2017 - $arraySum2016;
-      $arraySum2016 = $arraySum2016 - $arraySum2015;
-      $arraySum2015 = $arraySum2015 - $arraySum2014;
+        ?>
+      </tr>
+      <?php
+      foreach ($locations AS $location) {
+        $metersClass = new meters;
+        $meters = $metersClass->allByLocationAndType($location['uid'], 'Water');
 
-      $output  = "<tr>";
-			$output .= "<th scope=\"row\">" . $location['name'] . "</th>";
-      $output .= "<td>" . number_format($arraySum2015) . "</td>";
-			$output .= "<td>" . number_format($arraySum2016) . "</td>";
-			$output .= "<td>" . number_format($arraySum2017) . "</td>";
-			$output .= "<td>" . number_format($arraySum2018) . "</td>";
-			$output .= "<td>" . number_format($arraySum2019) . "</td>";
-      $output .= "<td>" . number_format($arraySum2020) . "</td>";
-			$output .= "</tr>";
-      
-			echo $output;
-		}
-		?>
-  </tbody>
-</table>
+        echo "<tr>";
+
+        echo "<th scope=\"row\">" . $location['name'] . "</th>";
+
+        $i = 5;
+        do {
+          $previousYear = date('Y', strtotime($i + 1 . " years ago"));
+          $year = date('Y', strtotime($i . " year ago"));
+
+          $meterMAXArrayPrevious = array();
+          $meterMAXArray = array();
+          foreach ($meters AS $meter) {
+            //echo "SELECT MAX(reading1) AS reading1 from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '" . $previousYear . "'";
+            $meterMAXPrevious = $db->query("SELECT MAX(reading1) AS reading1 from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '" . $previousYear . "'")->fetchAll();
+            $meterMAX = $db->query("SELECT MAX(reading1) AS reading1 from readings WHERE meter = '" . $meter['uid'] . "' AND YEAR(date) = '" . $year . "'")->fetchAll();
+            $meterMAXArrayPrevious[] = $meterMAXPrevious[0]['reading1'];
+            $meterMAXArray[] = $meterMAX[0]['reading1'];
+          }
+
+          $arraySum = array_sum($meterMAXArray) - array_sum($meterMAXArrayPrevious);
+
+          echo "<td>" . number_format($arraySum) . "</td>";
+          $i--;
+        } while ($i >= 0);
+
+        echo "</tr>";
+      }
+      ?>
+    </thead>
+  </table>
 </div>
