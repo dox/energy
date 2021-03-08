c949e9d090f366255150fa1cfe1769cfbbb38e12
Andrew Breakspear <andrew@longhanborough.com>
2021-03-08 15:54:48 +0000

    Including fav icons

--- index.php
+++ index.php
@@ -43,6 +43,24 @@
   <meta name="generator" content="Github Atom">
   <title>Utility Meter Readings</title>
 
+	<link rel="apple-touch-icon" sizes="57x57" href="/inc/favicons/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/inc/favicons/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/inc/favicons/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/inc/favicons/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/inc/favicons/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/inc/favicons/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/inc/favicons/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/inc/favicons/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/inc/favicons/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/inc/favicons/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/inc/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/inc/favicons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/inc/favicons/favicon-16x16.png">
+	<link rel="manifest" href="/inc/favicons/manifest.json">
+	<meta name="msapplication-TileColor" content="#212529">
+	<meta name="msapplication-TileImage" content="/inc/favicons/ms-icon-144x144.png">
+	<meta name="theme-color" content="#212529">
+
   <link rel="canonical" href="http://readings.seh.ox.ac.uk">
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
