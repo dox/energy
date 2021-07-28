9c08546c7d31b999b9c042ba938014d266b00df9
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2021-07-28 09:57:30 +0100

    including charts for site summary page

--- nodes/summary.php
+++ nodes/summary.php
@@ -11,11 +11,16 @@ $locations = $locations->all();
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
   <h1 class="h2">Site Summary</h1>
 </div>
-<?php
 
+<?php
 foreach (explode(",", $settingsClass->value('node_types')) AS $type) {
+  $chartData = array();
+  $chartName = "siteSummaryGraph_" . $type;
+
   $icon = "<svg width=\"1em\" height=\"1em\"><use xlink:href=\"inc/icons.svg#" . strtolower($type) . "\"/></svg>";
   $output  = "<h3>" . $icon . " " . $type . "</h3>";
+  $output .= displayGraph($chartName);
+
   $output .= "<table class=\"table mb-5\">";
   $output .= "<thead>";
   $output .= "<tr>";
@@ -56,6 +61,8 @@ foreach (explode(",", $settingsClass->value('node_types')) AS $type) {
 
       $arraySum = array_sum($meterMAXArray);
 
+      $chartData[$year] = $chartData[$year] + $arraySum;
+
       $output .= "<td>" . number_format($arraySum) . "</td>";
       $i--;
     } while ($i >= 0);
@@ -63,6 +70,63 @@ foreach (explode(",", $settingsClass->value('node_types')) AS $type) {
   }
   $output .= "</table>";
 
+  $scriptOutput .= "var timeFormat = 'YYYY/MM/DD';";
+  $scriptOutput .= "var config_meter_consumption_monthly = {";
+  $scriptOutput .= "type: 'bar',";
+  $scriptOutput .= "data: {";
+  $scriptOutput .= "labels: [ " . implode(",", array_keys($chartData)) . "],";
+  $scriptOutput .= "datasets: [{";
+  $scriptOutput .= "label: 'Monthly Consumption',";
+  $scriptOutput .= "backgroundColor: \"#3CB44B30\",";
+  $scriptOutput .= "borderColor: \"#3CB44B\",";
+  $scriptOutput .= "fill: true,";
+  $scriptOutput .= "data: [" . implode(",", $chartData) . "]";
+  $scriptOutput .= "}]";
+  $scriptOutput .= "},";
+  $scriptOutput .= "options: {";
+  $scriptOutput .= "plugins: {";
+  $scriptOutput .= "title: {";
+  $scriptOutput .= "text: 'Monthly Consumption',";
+  $scriptOutput .= "display: false";
+  $scriptOutput .= "},";
+  $scriptOutput .= "legend: {";
+  $scriptOutput .= "display: false,";
+  $scriptOutput .= "},";
+  $scriptOutput .= "},";
+  $scriptOutput .= "scales: {";
+  $scriptOutput .= "x: {";
+  $scriptOutput .= "title: {";
+  $scriptOutput .= "display: false";
+  $scriptOutput .= "}";
+  $scriptOutput .= "},";
+  $scriptOutput .= "y: {";
+  $scriptOutput .= "title: {";
+  $scriptOutput .= "display: true,";
+  $scriptOutput .= "text: '" . "Unit" . "'";
+  $scriptOutput .= "}";
+  $scriptOutput .= "}";
+  $scriptOutput .= "},";
+  $scriptOutput .= "}";
+  $scriptOutput .= "};";
+  $scriptOutput .= "";
+
+
   echo $output;
+
+  echo displayGraphJC($chartName, $chartData);
+
 }
+?>
+
+<script>
+<?php
+//echo $scriptOutput;
+?>
+</script>
+
+
+
+<?php
+
+
 ?>
