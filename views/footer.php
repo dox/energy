d989f5b06fcbcec3c265cc28344e0fb582244cb6
Andrew Breakspear <andrew@longhanborough.com>
2021-11-12 08:15:07 +0000

    Tidying up footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,18 @@
 <div class="container">
-	<footer class="text-muted pt-4 pb-4">
-		<p class="float-end"><a href="index.php?n=accessibility">Accessibility Statement</a></p>
-		<p><?php echo site_name; ?> is &copy;<a href="https://github.com/dox/energy">Andrew Breakspear</a>, but please download and customise it for yourself.</p>
+	<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+		<div class="col-md-4 d-flex align-items-center">
+			<span class="mb-3 me-2 mb-md-0 text-muted lh-1">
+				<svg class="bi" width="24" height="24" role="img" aria-label="<?php echo site_name; ?>"><use xlink:href="inc/icons.svg#logo"></use></svg>
+			</span>
+			<span class="text-muted">© <?php echo date('Y'); ?> <a href="https://github.com/dox" class="text-muted">Andrew Breakspear</a></span>
+		</div>
+		
+		<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+			<li class="ms-3">
+				<a class="text-muted" href="https://github.com/dox/energy">
+					<svg class="bi" width="24" height="24" role="img"><use xlink:href="inc/icons.svg#github"></use></svg>
+				</a>
+			</li>
+		</ul>
 	</footer>
 </div>
