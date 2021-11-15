d134859ef3de6fb4da1b8f4c2a7845701a09f3cd
Andrew Breakspear <andrew@longhanborough.com>
2021-11-15 14:04:36 +0000

    Basic consumption data for node export

--- export.php
+++ export.php
@@ -1,5 +1,17 @@
 <?php
 include_once("inc/include.php");
 
-printArray($_GET);
+if ($_GET['type'] == "node") {
+	$node = new meter($_GET['filter']);
+	
+	$thisYearDateFrom = date('Y-m-d', strtotime('100 months ago'));
+	$thisYearDateTo = date('Y-m-d');
+	$consumptionLast12Months = array_reverse($node->consumptionBetweenDatesByMonth($thisYearDateFrom, $thisYearDateTo), true);
+	
+	echo "consumption per month in " . $node->unit;
+	printArray($consumptionLast12Months);
+} else {
+	echo "<h2>This export hasn't been created yet.  It's on the roadmap to be developed though</h2>";
+	printArray($_GET);
+}
 ?>
